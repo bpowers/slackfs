c91e1e963fd4552f786ffc5bd1dd153da6d0f9b1
Bobby Powers <bobbypowers@gmail.com>
2015-07-01 16:58:52 -0400

    slack-sidebar: move core/boring UI stuff into ui_base.go

--- cmd/slack-sidebar/main.go
+++ cmd/slack-sidebar/main.go
@@ -7,136 +7,9 @@ import (
 	"github.com/nsf/termbox-go"
 )
 
-// Represent a position in terms of the terminal's columns (X) and
-// rows (Y)
-type Point struct {
-	X int
-	Y int
-}
-
-func (a Point) Add(b Point) Point {
-	return Point{a.X + b.X, a.Y + b.Y}
-}
-
-// Represent the size of a UI element in terms of the terminal's
-// columns (Width) and rows (Height)
-type Size struct {
-	Width  int
-	Height int
-}
-
-type Rect struct {
-	Point
-	Size
-}
-
-func NewRect(x, y, w, h int) Rect {
-	return Rect{Point{x, y}, Size{w, h}}
-}
-
-func (a Rect) Sub(b Rect) Rect {
-	// FIXME: this isn't general, it only works for the code I've
-	// written here.
-	if a.Y == b.Y {
-		return NewRect(a.X, a.Y+b.Height, a.Width, a.Height-b.Height)
-	} else if a.Y+a.Height == b.Y+b.Height {
-		return NewRect(a.X, a.Y, a.Width, a.Height-b.Height)
-	}
-	return a
-}
-
-type Box interface {
-	Size() Size
-	Resize(available Rect) (desired Rect)
-	NeedsDisplay() bool
-	Draw(bounds Rect)          // Draw yourself in these bounds
-	Handle(termbox.Event) bool // Event's MouseX/Y is Box-relative
-}
-
-type BoundedBox struct {
-	Bounds Rect
-	Box    Box
-}
-
-type Element struct {
-	size   Size
-	parent *Element
-	// if accessed from multiple goroutines, must be atomic
-	needsDisplay bool
-}
-
-type Container struct {
-	Element
-	children  []BoundedBox
-	canScroll bool // set only at initialization
-}
-
-type Window struct {
-	Container
-}
-
-func (e *Element) Size() Size {
-	return e.size
-}
-
-func (e *Element) NeedsDisplay() bool {
-	return e.needsDisplay
-}
-
-// A container fills up all available size.  If you want a header +
-// footer + flex content in the middle, add the header and footer
-// first to the parent.
-func (e *Container) Resize(available Rect) (desired Rect) {
-	e.size = available.Size
-
-	// children's Rects are relatively positioned inside the
-	// parent.
-	relBounds := Rect{Size: e.Size()}
-	for i, c := range e.children {
-		childBounds := c.Box.Resize(relBounds)
-		e.children[i].Bounds = childBounds
-		relBounds = relBounds.Sub(childBounds)
-	}
-
-	return Rect{desired.Point, e.Size()}
-}
-
-func (e *Container) Draw(bounds Rect) {
-	for _, c := range e.children {
-		c.Box.Draw(c.Bounds)
-	}
-}
-
-func (e *Container) Handle(ev termbox.Event) bool {
-	for _, c := range e.children {
-		if c.Box.Handle(ev) {
-			return true
-		}
-	}
-	return false
-}
-
-func (e *Container) AddChild(child Box) {
-	e.children = append(e.children, BoundedBox{Box: child})
-}
-
-func (e *Window) Paint() {
-	w, h := termbox.Size()
-	bounds := Rect{Point{0, 0}, Size{w, h}}
-
-	termbox.Clear(fgColor, bgColor)
-	e.Container.Draw(bounds)
-	termbox.Flush()
-}
-
-func (e *Window) Handle(ev termbox.Event) bool {
-	if ev.Type == termbox.EventResize {
-		e.needsDisplay = true
-		e.Resize(Rect{Point{0, 0}, Size{ev.Width, ev.Height}})
-		return true
-	}
-	return e.Container.Handle(ev)
-}
+const fgColor = termbox.ColorDefault
+const bgColor = termbox.ColorDefault
+const bold = termbox.AttrBold
 
 type Header struct {
 	Element
@@ -151,15 +24,15 @@ func (e *Header) Resize(available Rect) (desired Rect) {
 	return Rect{available.Point, e.Size()}
 }
 
-func (e *Header) Draw(bounds Rect) {
-	printString(bounds.Point, fgColor|bold, bgColor, "TEAM NAME")
+func (e *Header) Draw(view View) {
+	view.String(P(0, 0), fgColor|bold, bgColor, "TEAM NAME")
 
-	termbox.SetCell(0, 1, '●', termbox.ColorGreen, bgColor)
-	printString(Point{2, 1}, fgColor, bgColor, "Bobby Powers")
-	printString(Point{0, 3}, fgColor, bgColor, "CHANNELS")
-	printString(Point{0, 4}, fgColor, bgColor, "#general")
-	termbox.SetCell(19, 4, '✓', fgColor, bgColor)
-	termbox.SetCell(22, 4, '⊗', fgColor, bgColor)
+	view.SetCell(P(0, 1), '●', termbox.ColorGreen, bgColor)
+	view.String(P(2, 1), fgColor, bgColor, "Bobby Powers")
+	view.String(P(0, 3), fgColor, bgColor, "CHANNELS")
+	view.String(P(0, 4), fgColor, bgColor, "#general")
+	view.SetCell(P(19, 4), '✓', fgColor, bgColor)
+	view.SetCell(P(22, 4), '⊗', fgColor, bgColor)
 }
 
 type Footer struct {
@@ -177,8 +50,8 @@ func (e *Footer) Resize(available Rect) (desired Rect) {
 	return Rect{pos, e.Size()}
 }
 
-func (e *Footer) Draw(bounds Rect) {
-	printString(bounds.Point, fgColor|bold, bgColor, "--+--")
+func (e *Footer) Draw(view View) {
+	view.String(P(0, 0), fgColor|bold, bgColor, "--+--")
 }
 
 type Outline struct {
@@ -194,21 +67,11 @@ func (e *Outline) Resize(available Rect) (desired Rect) {
 	return Rect{available.Point, e.Size()}
 }
 
-func (e *Outline) Draw(bounds Rect) {
-	termbox.SetCell(bounds.X, bounds.Y, '┌', fgColor, bgColor)
-	termbox.SetCell(bounds.X, bounds.Y+bounds.Height-1, '└', fgColor, bgColor)
-	termbox.SetCell(bounds.X+bounds.Width-1, bounds.Y, '┐', fgColor, bgColor)
-	termbox.SetCell(bounds.X+bounds.Width-1, bounds.Y+bounds.Height-1, '┘', fgColor, bgColor)
-}
-
-const fgColor = termbox.ColorDefault
-const bgColor = termbox.ColorDefault
-const bold = termbox.AttrBold
-
-func printString(pos Point, fg, bg termbox.Attribute, msg string) {
-	for i, c := range msg {
-		termbox.SetCell(pos.X+i, pos.Y, c, fg, bg)
-	}
+func (e *Outline) Draw(view View) {
+	view.SetCell(P(0, 0), '┌', fgColor, bgColor)
+	view.SetCell(P(0, -1), '└', fgColor, bgColor)
+	view.SetCell(P(-1, 0), '┐', fgColor, bgColor)
+	view.SetCell(P(-1, -1), '┘', fgColor, bgColor)
 }
 
 func main() {
@@ -237,8 +100,7 @@ func main() {
 	window.AddChild(new(Outline))
 
 	w, h := termbox.Size()
-	size := Size{w, h}
-	window.Resize(Rect{Point{0, 0}, size})
+	window.Resize(NewRect(0, 0, w, h))
 
 	window.Paint()
 
@@ -251,6 +113,10 @@ func main() {
 		}
 
 		window.Handle(ev)
+		if window.NeedsResize() {
+			window.Resize(Rect{Point{0, 0}, window.Size()})
+		}
+
 		if window.NeedsDisplay() {
 			window.Paint()
 		}
---
+++ cmd/slack-sidebar/ui_base.go
@@ -0,0 +1,195 @@
+package main
+
+import "log"
+
+import "github.com/nsf/termbox-go"
+
+// Represent a position in terms of the terminal's columns (X) and
+// rows (Y)
+type Point struct {
+	X int
+	Y int
+}
+
+func (a Point) Add(b Point) Point {
+	return Point{a.X + b.X, a.Y + b.Y}
+}
+
+// Shorthand for Point{X, Y}
+func P(x, y int) Point {
+	return Point{x, y}
+}
+
+// Represent the size of a UI element in terms of the terminal's
+// columns (Width) and rows (Height)
+type Size struct {
+	Width  int
+	Height int
+}
+
+// Returns true if _either_ the width or height is zero.  If either
+// are zero, we can't be expected to see any output in this region.
+func (s Size) Empty() bool {
+	return s.Width == 0 || s.Height == 0
+}
+
+type Rect struct {
+	Point
+	Size
+}
+
+func NewRect(x, y, w, h int) Rect {
+	return Rect{Point{x, y}, Size{w, h}}
+}
+
+func (r Rect) Contains(p Point) bool {
+	return r.X <= p.X && r.Y <= p.Y &&
+		p.X < r.X+r.Width && p.Y < r.Y+r.Height
+}
+
+func (a Rect) Sub(b Rect) Rect {
+	// FIXME: this isn't general, it only works for the code I've
+	// written here.
+	if a.Y == b.Y {
+		return NewRect(a.X, a.Y+b.Height, a.Width, a.Height-b.Height)
+	} else if a.Y+a.Height == b.Y+b.Height {
+		return NewRect(a.X, a.Y, a.Width, a.Height-b.Height)
+	}
+	return a
+}
+
+type Box interface {
+	Size() Size
+	NeedsResize() bool
+	Resize(available Rect) (desired Rect)
+	NeedsDisplay() bool
+	Draw(View)                 // Draw yourself into this view
+	Handle(termbox.Event) bool // Event's MouseX/Y is Box-relative
+}
+
+type BoundedBox struct {
+	Bounds Rect
+	Box    Box
+}
+
+type View struct {
+	bounds Rect // bounds we're allowed to draw in
+}
+
+// allow intuitive use of negative offsets
+func (v *View) normalize(pos Point) Point {
+	var result Point
+	if pos.X >= 0 {
+		result.X = v.bounds.X + pos.X
+	} else {
+		result.X = v.bounds.X + v.bounds.Width + pos.X
+	}
+	if pos.Y >= 0 {
+		result.Y = v.bounds.Y + pos.Y
+	} else {
+		result.Y = v.bounds.Y + v.bounds.Height + pos.Y
+	}
+	return result
+}
+
+func (v *View) SetCell(relPos Point, ch rune, fg, bg termbox.Attribute) {
+	pos := v.normalize(relPos)
+	if !v.bounds.Contains(pos) {
+		log.Printf("SetCell(%#v, %s) OOB: %#v", relPos, ch, v.bounds)
+		return
+	}
+	termbox.SetCell(pos.X, pos.Y, ch, fg, bg)
+}
+
+func (v *View) String(relPos Point, fg, bg termbox.Attribute, msg string) {
+	for i, c := range msg {
+		v.SetCell(P(relPos.X+i, relPos.Y), c, fg, bg)
+	}
+}
+
+type Element struct {
+	size Size
+	// if accessed from multiple goroutines, must be atomic
+	needsDisplay bool
+	needsResize  bool
+}
+
+type Container struct {
+	Element
+	children  []BoundedBox
+	canScroll bool // set only at initialization
+}
+
+func (e *Element) Size() Size {
+	return e.size
+}
+
+func (e *Element) NeedsDisplay() bool {
+	return e.needsDisplay
+}
+
+func (e *Element) NeedsResize() bool {
+	return e.needsResize
+}
+
+// A container fills up all available size.  If you want a header +
+// footer + flex content in the middle, add the header and footer
+// first to the parent.
+func (e *Container) Resize(available Rect) (desired Rect) {
+	e.needsResize = false
+	e.size = available.Size
+
+	// children's Rects are relatively positioned inside the
+	// parent.
+	relBounds := Rect{Size: e.Size()}
+	for i, c := range e.children {
+		childBounds := c.Box.Resize(relBounds)
+		e.children[i].Bounds = childBounds
+		relBounds = relBounds.Sub(childBounds)
+	}
+
+	return Rect{desired.Point, e.Size()}
+}
+
+func (e *Container) Draw(view View) {
+	for _, c := range e.children {
+		c.Box.Draw(View{c.Bounds})
+	}
+}
+
+func (e *Container) Handle(ev termbox.Event) bool {
+	for _, c := range e.children {
+		if c.Box.Handle(ev) {
+			return true
+		}
+	}
+	return false
+}
+
+func (e *Container) AddChild(child Box) {
+	e.children = append(e.children, BoundedBox{Box: child})
+}
+
+type Window struct {
+	Container
+}
+
+func (e *Window) Paint() {
+	e.needsDisplay = false
+
+	w, h := termbox.Size()
+	bounds := NewRect(0, 0, w, h)
+
+	termbox.Clear(fgColor, bgColor)
+	e.Container.Draw(View{bounds})
+	termbox.Flush()
+}
+
+func (e *Window) Handle(ev termbox.Event) bool {
+	if ev.Type == termbox.EventResize {
+		e.needsDisplay = true
+		e.Resize(NewRect(0, 0, ev.Width, ev.Height))
+		return true
+	}
+	return e.Container.Handle(ev)
+}
